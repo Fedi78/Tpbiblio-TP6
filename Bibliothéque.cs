4835db0773ea7e292b4eaebf298dc3c330aa38c9
Fedi78 <fedidou78@gmail.com>
2023-03-30 09:20:29 +0200

    Ajoutez des fichiers projet.

---
+++ Bibliothéque.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpbiblio
+{
+    public class Bibliothèque
+    {
+        public string Nom { get; set; }
+        public List<Livre> Livres { get; set; }
+
+        public Bibliothèque(string nom)
+        {
+            Nom = nom;
+            Livres = new List<Livre>();
+        }
+
+        public Livre LivreExiste(string titre)
+        {
+            foreach (Livre livre in Livres)
+            {
+                if (livre.Titre == titre)
+                {
+                    return livre;
+                }
+            }
+            return null;
+        }
+
+        public void AjouterLivre(Livre livre)
+        {
+            Livres.Add(livre);
+        }
+
+        public int Cardinal()
+        {
+            return Livres.Count;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Bibliothèque: {Nom}");
+            sb.AppendLine("Liste des livres:");
+            foreach (Livre livre in Livres)
+            {
+                sb.AppendLine(livre.ToString());
+            }
+            return sb.ToString();
+        }
+
+        public void AfficheParAuteur(string auteur)
+        {
+            foreach (Livre livre in Livres)
+            {
+                if (livre.Auteur == auteur)
+                {
+                    Console.WriteLine(livre.ToString());
+                }
+            }
+        }
+
+        public void AfficheParEditeur(string editeur)
+        {
+            foreach (Livre livre in Livres)
+            {
+                if (livre.Editeur == editeur)
+                {
+                    Console.WriteLine(livre.ToString());
+                }
+            }
+        }
+
+        public bool SupprimerLivre(string titre)
+        {
+            Livre livre = LivreExiste(titre);
+            if (livre != null)
+            {
+                Livres.Remove(livre);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void SupprimeLivreAuteur(string auteur)
+        {
+            Livres.RemoveAll(livre => livre.Auteur == auteur);
+        }
+    }
+
+}
---
+++ Livre.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpbiblio
+{
+    public class Livre
+    {
+        private string code;
+        private string titre;
+        private int annee;
+        private string editeur;
+        private string auteur;
+
+        public Livre(string co, string ti, int an, string ed, string aut)
+        {
+            code = co;
+            titre = ti;
+            annee = an;
+            editeur = ed;
+            auteur = aut;
+        }
+
+        public string Code
+        {
+            get { return code; }
+            set { code = value; }
+        }
+
+        public string Titre
+        {
+            get { return titre; }
+            set { titre = value; }
+        }
+
+        public int Annee
+        {
+            get { return annee; }
+            set { annee = value; }
+        }
+
+        public string Editeur
+        {
+            get { return editeur; }
+            set { editeur = value; }
+        }
+
+        public string Auteur
+        {
+            get { return auteur; }
+            set { auteur = value; }
+        }
+
+        public override string ToString()
+        {
+            return "Code: " + code + "\nTitre: " + titre + "\nAnnée: " + annee + "\nÉditeur: " + editeur + "\nAuteur: " + auteur;
+        }
+    }
+
+}
